9705d4d455f5478b49eeb67078acdbf4a4d34206
서지호 <103229466+jihoseo2006@users.noreply.github.com>
2024-07-30 13:36:16 +0900

    기능 추가하기
    감소, 초기화 버튼 생성

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,11 +2,13 @@ import { userCounterStore } from "../store";
 
 
 export default function Counter() {
-  const {count, inc} = userCounterStore()
+  const {count, inc, dec, res} = userCounterStore()
   return (
     <div className="counter">
       <p>{count}</p>
       <button onClick={inc}>one up</button>
+      <button onClick={dec}>one down</button>
+      <button onClick={res}>reset zero</button>
     </div>
   )
 }
--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import { create }from "zustand";
 
 
 export const userCounterStore = create((set) => ({
-  count: 1,
+  count: 0,
   inc: () => set((state) => ({count: state.count + 1})),
+  dec: () => set((state) => ({count: state.count - 1})),
+  res: () => set((state) => ({count: state.count = 0})),
 }))
